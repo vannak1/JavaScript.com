8b2f4ef1d35de650eba9470870233e995c106253
Carlos Souza <carloshrsouza@gmail.com>
2015-02-17 08:48:38 -0500

    Add new object literals syntax

--- services/flow.js
+++ services/flow.js
@@ -2,21 +2,21 @@ var db = require('./db');
 
 var Flow = {
   // Returns the most recent Flow items for display on the index page
-  recent: function (cb) {
+  recent(cb) {
     db.query('SELECT * FROM articles WHERE news = false ORDER BY created_at DESC LIMIT 10', [], cb)
   },
 
   // Returns all Flow items, no pagination
-  all: function (cb) {
+  all(cb) {
     db.query('SELECT * FROM articles WHERE news = false', [], cb)
   },
 
-  byID: function(id, cb) {
+  byID(id, cb) {
     db.query('SELECT * FROM articles WHERE news = false and id = $1', [id], cb)
   },
 
   // Creates a new Flow item
-  create: function (newFlow, cb) {
+  create(newFlow, cb) {
     var title = newFlow.title;
     var body = newFlow.body;
     var url = newFlow.url;
