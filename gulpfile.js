0349b90201795417f370b92616383aec4c867d40
Drew Barontini <drewbarontini@gmail.com>
2015-02-19 12:59:25 -0500

    Add 'gulp-minify-css'

--- gulpfile.js
+++ gulpfile.js
@@ -8,39 +8,59 @@
 //   Modules
 // -------------------------------------
 
-var gulp       = require( 'gulp' );
-    watch      = require( 'gulp-watch' ),
-    sass       = require('gulp-sass');
+var gulp      = require('gulp');
+var watch     = require('gulp-watch');
+var sass      = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var rename    = require('gulp-rename');
 
 // -------------------------------------
 //   Variables
 // -------------------------------------
 
-var sassFiles       = [ 'public/stylesheets/*.sass', 'public/stylesheets/**/*.sass' ];
-var cssFiles        = 'public/stylesheets';
+var options = {
+
+  css: {
+    files: 'public/stylesheets',
+    file: 'public/stylesheets/application.css',
+    destination: 'public/stylesheets'
+  },
+
+  sass: {
+    files: ['public/stylesheets/*.sass', 'public/stylesheets/**/*.sass'],
+    destination: 'public/stylesheets'
+  }
+
+};
 
 // -------------------------------------
 //   Task: Default
 // -------------------------------------
 
-gulp.task( 'default', function() {
+gulp.task('default', function() {
+  watch(options.sass.files, function(files) {
+    gulp.start('sass');
+    gulp.start('minify-css');
+  });
+});
 
-  watch( sassFiles, function( files ) {
+// -------------------------------------
+//   Task: CSS Minify
+// -------------------------------------
 
-    gulp.start( 'sass' );
-
-  } );
-
-} );
+gulp.task('minify-css', function () {
+  gulp.src(options.css.file)
+      .pipe(minifycss())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest(options.css.destination));
+});
 
 // -------------------------------------
 //   Task: Sass
 // -------------------------------------
 
-gulp.task( 'sass', function () {
-
-  gulp.src( sassFiles )
-      .pipe( sass( { indentedSyntax: true } ) )
-      .pipe( gulp.dest( cssFiles ) );
-
-} );
+gulp.task('sass', function () {
+  gulp.src(options.sass.files)
+      .pipe(sass({ indentedSyntax: true }))
+      .pipe(gulp.dest(options.sass.destination));
+});
