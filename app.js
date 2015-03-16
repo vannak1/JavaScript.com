c289735fecf4c154650cb74774818a872159fa38
Drew Barontini <drewbarontini@gmail.com>
2015-03-16 14:13:47 -0400

    Add /styleguide static page
    This page is added for the front-end buildout.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var news = require('./routes/news');
 var flow = require('./routes/flow');
+var styleguide = require('./routes/styleguide');
 
 var app = express();
 
@@ -26,10 +27,12 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', routes);
 app.use('/users', users);
 app.use('/news', news);
 app.use('/flow', flow);
+app.use('/styleguide', styleguide);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
