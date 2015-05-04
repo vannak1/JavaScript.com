5b39990e5ffc39b636a9426b7ead6f66c79f25b4
John D. Jameson <thejohnjameson@gmail.com>
2015-05-04 08:46:11 -0400

    Add 404 page

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
-var news = require('./routes/news');
-var flow = require('./routes/flow');
-var styleguide = require('./routes/styleguide');
-var courses = require('./routes/courses');
-var learn = require('./routes/learn');
-var resources = require('./routes/resources');
+
 var brand = require('./routes/brand');
+var courses = require('./routes/courses');
+var flow = require('./routes/flow');
+var learn = require('./routes/learn');
+var news = require('./routes/news');
+var notFound = require('./routes/notFound');
+var resources = require('./routes/resources');
+var styleguide = require('./routes/styleguide');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -33,15 +35,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 app.use('/', routes);
+app.use('/404', notFound)
+app.use('/brand', brand);
+app.use('/courses', courses)
+app.use('/courses.json', courses);
+app.use('/flow', flow);
+app.use('/news', news);
+app.use('/resources', resources)
+app.use('/styleguide', styleguide);
 app.use('/try', learn)
 app.use('/users', users);
-app.use('/news', news);
-app.use('/flow', flow);
-app.use('/styleguide', styleguide);
-app.use('/courses.json', courses);
-app.use('/courses', courses)
-app.use('/resources', resources)
-app.use('/brand', brand);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
