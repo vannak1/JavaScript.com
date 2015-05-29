ebd5281d706784a6e297c5a68651baed0a89ef4f
joeltaylor <joel@codeschool.com>
2015-05-29 10:40:14 -0400

    Temporary e-mail fix

--- services/github-api.js
+++ services/github-api.js
@@ -2,26 +2,30 @@ var request = require('request');
 
 var GithubApi = {
   fetchEmail(profile, token, cb) {
-    var options = {
-      headers: {
-        'User-Agent':    baseURL,
-        'Authorization': 'token ' + token,
-        'x-oauth-scopes': 'user:email'
-      },
-      json:    true,
-      url:     'https://api.github.com/user/emails'
-    };
+    //TODO: Find out why this call doesn't work 
+    //
+    // var options = {
+    //   headers: {
+    //     'User-Agent':    baseURL,
+    //     'Authorization': 'token ' + token,
+    //     'x-oauth-scopes': 'user:email'
+    //   },
+    //   json:    true,
+    //   url:     'https://api.github.com/user/emails'
+    // };
 
-    // get emails using oauth token
-    request(options, function(error, response, body) {
-      var emails = body.filter(function(email) {
-        return (email.verified && email.primary);
-        return email.verified;
-      });
+    // // get emails using oauth token
+    // request(options, function(error, response, body) {
+    //   var emails = body.filter(function(email) {
+    //     return (email.verified && email.primary);
+    //     return email.verified;
+    //   });
 
-      profile.email = emails[0].email;
-      cb(profile);
-    });
+    //   profile.email = emails[0].email;
+    //   cb(profile);
+    // });
+    profile.email = 'example@email.com';
+    cb(profile);
   }
 }
 module.exports = GithubApi;
