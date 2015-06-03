5ca32e69cfb7134f16631a519250570e2277e18c
Drew Barontini <drewbarontini@gmail.com>
2015-06-03 08:03:57 -0400

    Set up dispatcher
    The Dispatcher Class will handle running the appropriate per-page code
    based on the 'data-dispatcher-page' attribute on the 'body'.

---
+++ client/javascripts/components/dispatcher/news/new.js
@@ -0,0 +1,29 @@
+// *************************************
+//
+//   News New
+//   -> Dispatch events
+//
+// *************************************
+
+JS.Pages.News.New = function() {
+
+  // -------------------------------------
+  //   Modules
+  // -------------------------------------
+
+  JS.Modules.Counter.init({
+    onMinPreceeded: function(settings) {
+      $('.js-counter-message-min').removeClass('is-hidden');
+      $('.js-counter-message-max').addClass('is-hidden');
+    },
+    onMaxExceeded: function(settings) {
+      $('.js-counter-message-max').removeClass('is-hidden');
+      $('.js-counter-message-min').addClass('is-hidden');
+    },
+    onConditionsMet: function(settings){
+      $('.js-counter-message-min').addClass('is-hidden');
+      $('.js-counter-message-max').addClass('is-hidden');
+    }
+  });
+
+};
