a5ea6be2bb86c56a6ac7af9572e73e15af9a55fd
joeltaylor <joel@codeschool.com>
2015-06-03 10:31:25 -0400

    Only show approved comments

--- services/comments.js
+++ services/comments.js
@@ -4,13 +4,12 @@ var Akismetor = require('../services/akismetor');
 var Comments = {
   // Returns all comments by article with user information.
   findByArticleId(id, cb) {
-    db.query('SELECT c.id, c.created_at, c.body, u.avatar_url, u.name FROM comments as c JOIN users as u on u.id = c.user_id WHERE c.article_id = $1 ORDER BY created_at ASC;', [ id ], cb)
+    db.query('SELECT c.id, c.created_at, c.body, u.avatar_url, u.name FROM comments as c JOIN users as u on u.id = c.user_id WHERE c.article_id = $1 AND approved = true ORDER BY created_at ASC;', [ id ], cb)
   },
   // Creates a new comment
   create(newComment, cb) {
 
     var approved = newComment.isSpam ? false : true;
-    console.log(newComment);
     db.query(
       "INSERT INTO comments (user_id, approved, body, article_id) VALUES ($1, $2, $3, (SELECT id FROM articles WHERE slug = $4)) RETURNING id;",
       [newComment.userId, approved, newComment.body, newComment.slug],
