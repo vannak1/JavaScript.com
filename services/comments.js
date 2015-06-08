60bdca04dcd96230f34257e2e9aac0ffdae78f34
joeltaylor <joel@codeschool.com>
2015-06-08 13:42:48 -0400

    Return user_id with each comment

--- services/comments.js
+++ services/comments.js
@@ -4,7 +4,7 @@ var Akismetor = require('../services/akismetor');
 var Comments = {
   // Returns all comments by article with user information.
   findByArticleId(id, cb) {
-    db.query('SELECT c.id, c.created_at, c.body, u.avatar_url, u.name FROM comments as c JOIN users as u on u.id = c.user_id WHERE c.article_id = $1 AND approved = true ORDER BY created_at ASC;', [ id ], cb)
+    db.query('SELECT c.id, c.created_at, c.body, c.user_id, u.avatar_url, u.name FROM comments as c JOIN users as u on u.id = c.user_id WHERE c.article_id = $1 AND approved = true ORDER BY created_at ASC;', [ id ], cb)
   },
 
   findByCommentId(id, cb) {
