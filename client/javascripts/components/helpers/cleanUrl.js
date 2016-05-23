a912746b5976b3c57486a8eb917a22884b97e83a
John D. Jameson <thejohnjameson@gmail.com>
2016-05-23 15:23:07 -0400

    Add cleanUrl helper script

---
+++ client/javascripts/components/helpers/cleanUrl.js
@@ -0,0 +1,21 @@
+// *************************************
+//
+//   Clean URL
+//   -> Strip URL parameters
+//
+// *************************************
+
+JS.Helpers.cleanUrl = function(url) {
+  let anchor = document.createElement('a');
+
+  anchor.href = url;
+
+  return anchor.protocol + '//' + anchor.host + anchor.pathname;
+}
+
+// -------------------------------------
+//   Usage
+// -------------------------------------
+//
+// JS.Helpers.cleanUrl('https://example.com/?foo=bar#baz')
+//
